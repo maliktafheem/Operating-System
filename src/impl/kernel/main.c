78d485282926b7946c95a4e32e8bd51ba01e92a8
Malik Tafheem <maliktafheem10@gmail.com>
2021-04-21 10:58:23 +0500

    1st commit

---
+++ src/impl/kernel/main.c
@@ -0,0 +1,29 @@
+#include "print.h"
+
+void kernel_main() {
+    print_clear();
+    print_set_color(PRINT_COLOR_LIGHT_CYAN, PRINT_COLOR_BLACK);
+    
+print_str("\n%####%,        ######%% #%######%     (######%     /%%%%%%#.   #############%%%%");
+print_str("\n    %%%%,         #%       %%%           #%     .%%       %#   %#     #%%     .%");
+print_str("\n    %% %%%*       #%       %%#           #%     %%%       ./   (      #%%      /");
+print_str("\n    %%   %%%*     (%       %%#           #%      %%%%%/               #%%       ");
+print_str("\n    %%     %%%/   /%       %%#           #%         .%%%%%%           #%%       ");
+print_str("\n    %%       #%%( /%       %%%           #%              %%%/         #%%       ");
+print_str("\n    %%         #%%%%       #%%           %(     %#        %%#         #%%       ");
+print_str("\n    %%           #%%        (%%%(.    (%%,      %%%,    ,%%*          %%%       ");
+print_str("\n                                                                                ");
+print_set_color(PRINT_COLOR_LIGHT_GREEN, PRINT_COLOR_BLACK);
+print_str("\n                                        ,                                       ");
+print_str("\n                                    #   %  .%                                   ");
+print_str("\n                             .,*,,%, , *%  / (%####(                            ");
+print_str("\n                         .*   ,(  % %, %%( /% ( .%   .#                         ");
+print_str("\n                           *%%.  #%%%,.%%% /%%%*  /%%                           ");
+print_str("\n                       , ,*  (%%%( .%,(%%%*/%  %%%%.  % #                       ");
+print_str("\n                       .%# %%  %%%%%%.#*,*#*%%%%%* /%,.%%*                      ");
+print_str("\n                     #(   *#%%%( %%   #%%%(   %# %%%%%%/.  #                    ");
+print_str("\n                             ,(%%%  %%%%%%%%%  %%%%#*.                          ");
+print_set_color(PRINT_COLOR_LIGHT_RED, PRINT_COLOR_BLACK);
+print_str("\n                                ENCODED BY: TAFHEEM                             ");
+
+}
